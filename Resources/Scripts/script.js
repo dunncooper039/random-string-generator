d0049fcd7ad7bf19056e5da4d7e42161af6d414e
dunncooper039 <dunncooper039@gmail.com>
2024-09-01 22:11:04 -0500

    Improved randomness, added possibilities

--- Resources/Scripts/script.js
+++ Resources/Scripts/script.js
@@ -1,11 +1,19 @@
 import { subjects } from "./subjects.js";
 import { actions } from "./actions.js";
 import { commentary } from "./commentary.js";
+function randomFromArray(array) {
+  const doubleArray = array.concat(array)
+  const randomBuffer = new Uint32Array(1);
+  window.crypto.getRandomValues(randomBuffer);
+  let randomNumber = randomBuffer[Math.floor(Math.random() * (randomBuffer.length))] / (0xffffffff + 1);
+  return doubleArray[Math.floor(randomNumber * doubleArray.length)];
+}
 const generateSentence = () => {
-  const subject = subjects[Math.floor(Math.random() * (subjects.length))];
-  const action = actions[Math.floor(Math.random() * (actions.length))].replace("POSESSIVE", subject.posessive);
-  const comment = commentary[Math.floor(Math.random() * (commentary.length))];
-  document.getElementById('output').innerHTML = subject.text + ' ' + action + subject.punctuation + ' ' + comment;
+  const subject = randomFromArray(subjects);
+  const action = randomFromArray(actions).replace("POSESSIVE", subject.posessive);
+  const comment = randomFromArray(commentary);
+  const output = subject.text + ' ' + action + subject.punctuation + ' ' + comment;
+  document.getElementById('output').innerHTML = output;
   console.log('There are ' + (subjects.length * actions.length * commentary.length) + ' possible outputs at this time.');
 }
 generate.onclick = generateSentence;
