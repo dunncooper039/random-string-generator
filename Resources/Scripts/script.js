6fed2fc78484e3e6afbe8f160f647d07916a4536
Cooper Dunn <dunncooper039@gmail.com>
2026-06-16 20:31:50 -0500

    Simplified array randomization cause I was lowkey doing too much

--- Resources/Scripts/script.js
+++ Resources/Scripts/script.js
@@ -13,24 +13,13 @@ generate.textContent = "Generate";
 //Log the number of possible outputs to the console
 console.log('There are ' + (subjects.length * actions.length * commentary.length) + ' possible outputs at this time.');
 
-//This is an attempt to reduce randomization bias, it shuffles an array
-function shuffleArray(array) {
-  let newArray = array
-  for (let i = newArray.length - 1; i > 0; i--) {
-      const j = Math.floor(Math.random() * (i + 1));
-      [newArray[i], newArray[j]] = [newArray[j], newArray[i]];
-  }
-  return newArray;
-}
 //Gets random item from array
 function randomFromArray(array) {
-  //Shuffle the array and apend it to itself to randomize the order somewhat more and reduce bias
-  const doubleArray = shuffleArray(array).concat(shuffleArray(array))
-  //Use crypto to select an item from the new array and return it
+  //Use crypto to select an item from the array and return it
   const randomBuffer = new Uint32Array(1);
   window.crypto.getRandomValues(randomBuffer);
   let randomNumber = randomBuffer[Math.floor(Math.random() * (randomBuffer.length))] / (0xffffffff + 1);
-  return doubleArray[Math.floor(randomNumber * doubleArray.length)];
+  return array[Math.floor(randomNumber * array.length)];
 }
 //Chains fragments together
 const generateSentence = () => {
